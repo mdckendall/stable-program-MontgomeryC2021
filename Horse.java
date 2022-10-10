c2b844b408ac189c3993d4fe813b178389a195d5
MontgomeryC2021 <101913290+MontgomeryC2021@users.noreply.github.com>
2022-10-09 21:38:43 -0500

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -2,5 +2,9 @@ class Horse {
   public String name = "[No Name Set]";
   public double weight = 0.00d;
   public boolean tame = false;
-  public Horse(){}
+  public Horse( String hn, double w, boolean t){
+    this.name = hn;
+    this.weight = w;
+    this.tame = t;
+  }
 }
