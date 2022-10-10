4ea5047dc8813a9beb4a70722c5950ae5419014a
MontgomeryC2021 <101913290+MontgomeryC2021@users.noreply.github.com>
2022-10-09 21:40:28 -0500

    Update Stable.java

--- Stable.java
+++ Stable.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 class Stable {
-  public ArrayList<Horse> horses = new ArrayList<Horse>();
+  public ArrayList<Horse> arrayList = new ArrayList<Horse>();
   public String address = "[No Address Set]";
   public Stable(){}
 }
